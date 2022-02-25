6aa131b5a8937a9210a2e589e35e4a5b88ad98d3
Damian Żygadło <vvwill4u@gmail.com>
2022-02-25 02:11:27 +0100

    Updated the deps to the latest versions

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,7 +5,7 @@ import { styles } from './App.css';
 export const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx ziom to start working on your app!</Text>
+      <Text>Open up App.tsx ziomek to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
