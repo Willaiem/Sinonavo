3b6fe50b2396b760c35e74770168382b6257d667
Damian Żygadło <vvwill4u@gmail.com>
2022-03-15 11:32:40 +0100

    refactor: changed the way of passing arguments

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -2,10 +2,12 @@ import { useEffect } from "react";
 
 import { debounce } from "../utils/debounce";
 
-export const useDebounce = <A = unknown, R = void>(
+export const useDebounce = <A = unknown, R = void>({
+  fn, ms
+}: {
   fn: (args?: A) => R,
   ms?: number
-): ((args?: A) => void) => {
+}): ((args?: A) => void) => {
   const { debouncedFn, destroyTimer } = debounce<A, R>(fn, ms);
 
   useEffect(() => destroyTimer(), []);
