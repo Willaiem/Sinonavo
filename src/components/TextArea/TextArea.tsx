e2978857f48c6571f504be730413f5301969e67c
Damian Żygadło <vvwill4u@gmail.com>
2022-03-15 11:33:13 +0100

    refactor: cleaned up the text area component

--- src/components/TextArea/TextArea.tsx
+++ src/components/TextArea/TextArea.tsx
@@ -1,30 +1,10 @@
-import { useState } from "react"
 import { TextInput, TextInputProps } from "react-native"
 import { styles } from "./TextArea.css"
 
-const useFocus = () => {
-  const [isFocused, setIsFocused] = useState(false)
-
-  const onFocus = () => {
-    setIsFocused(true)
-  }
-  const onBlur = () => {
-    setIsFocused(false)
-  }
-
-  return { isFocused, onFocus, onBlur }
-}
-
 export const TextArea = (props: TextInputProps) => {
-  const { isFocused, ...events } = useFocus()
-
   return (
     <TextInput
-      {...events}
       {...props}
-      onSelectionChange={e =>
-        console.log("selection change", e.nativeEvent.selection)
-      }
       multiline
       numberOfLines={10}
       style={styles.textArea}
